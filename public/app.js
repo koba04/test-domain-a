25a4b9ab2734e35fbc80a977d8018a32691cfe9a
Toru Kobayashi <koba0004@gmail.com>
2018-01-16 17:29:37 +0900

    test Storage Access API

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 'use strict';
 
-const main = document.querySelector('main');
-main.textContent = 'Hello from app.js';
+const p = document.querySelector('p');
+p.textContent = 'Hello from app.js';
+
+if (typeof document.hasStorageAccess === 'function') {
+  console.log('Your browser supports Storage Access API');
+  document.hasStorageAccess().then((res) => {
+    p.textContent = 'document.hasStorageAccess returns ' + res;
+  });
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  if (typeof document.requestStorageAccess === 'function') {
+    document.requestStorageAccess().then(
+      () => p.textContent = 'requestStorageAccess successed!',
+      () => p.textContent = 'requestStorageAccess failed!'
+    );
+  }
+});
